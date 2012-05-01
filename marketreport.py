16f762f40512c0d006017f9f64908c2b597e5a51
James Burns <jfburns@gmail.com>
2012-05-01 15:36:14 -0700

    Completely redone to use eveapi interface. Starting correlation of market orders.

--- marketreport.py
+++ marketreport.py
@@ -1,15 +1,41 @@
-import urllib2, urllib
+import imp, argparse
 import settings
+eveapi = imp.load_source('eveapi', '../eveapi/eveapi.py')
 
-def getcharacters(keyID, vCode):
-    values = {}
-
-    values['keyID'] = keyID
-    values['vCode'] = vCode
-
-    encvalues = urllib.urlencode(values)
-
-    print urllib2.urlopen('https://api.eveonline.com/account/Characters.xml.aspx?',encvalues).read()
+market_transaction = 2
+brokers_fee = 46
+transaction_tax = 54
 
 if __name__ == '__main__':
-    getcharacters(settings.keyID,settings.vCode)
+    parser = argparse.ArgumentParser(description='Options')
+    parser.add_argument('-n', '--name', dest='name', default=settings.name)
+    args = parser.parse_args()
+
+    api = eveapi.EVEAPIConnection()
+    auth = api.auth(keyID=settings.keyID, vCode=settings.vCode)
+
+    result = auth.account.Characters()
+
+    for character in result.characters:
+        if character.name==args.name:
+            accountbalance = auth.char.AccountBalance(characterID=character.characterID)
+            print accountbalance.accounts[0].balance
+            walletjournal = auth.char.WalletJournal(characterID=character.characterID)
+            transactionsByRefTypeID = walletjournal.transactions.GroupedBy('refTypeID')
+            transactionsByDate = walletjournal.transactions.GroupedBy('date')
+            for transaction in transactionsByRefTypeID[market_transaction]:
+                sourceid = transaction.refID
+                print 'Finding transaction correlated to {0} for {1}'.format(sourceid, transaction.amount)
+                sourcedate = transaction.date
+                samedate = transactionsByDate[sourcedate]
+                for trans in samedate:
+                    if trans.refTypeID == brokers_fee:
+                        print 'Paid broker {0}'.format(trans.amount)
+                    elif trans.refTypeID == transaction_tax:
+                        print 'Paid transaction tax {0}'.format(trans.amount)
+                
+            #marketorders = auth.char.MarketOrders(characterID=character.characterID)
+            #ordersByTypeID = marketorders.orders.GroupedBy('typeID')
+            #for order in ordersByTypeID[439]:
+            #    print order
+
--- settings_template.py
+++ settings_template.py
@@ -1,2 +1,3 @@
 keyID = ''
 vCode = ''
+name = ''
