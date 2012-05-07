f3fca0209a96c067cac9de0ef4ee82344a4c211d
James Burns <jfburns@gmail.com>
2012-05-07 14:57:02 -0700

    adding transaction walking

--- marketupdate.py
+++ marketupdate.py
@@ -1,3 +1,6 @@
+from calendar import timegm
+from datetime import datetime
+from datetime import timedelta
 import imp, argparse
 import settings
 import time
@@ -17,7 +20,33 @@ if __name__ == '__main__':
 
     for character in result.characters:
         if character.name==args.name:
-            wtrans = auth.char.WalletTransactions(characterID=character.characterID)
+            wtrans = auth.char.WalletTransactions(characterID=character.characterID, rowCount=2560)
+
+            now = datetime.utcnow()
+            today = datetime(now.year,now.month,now.day,23,59,59)
+            todaysec = timegm(today.utctimetuple())
+            last = today - timedelta(settings.valuePeriod)
+            oldest = todaysec
+            oldestID = 0
 
             for row in wtrans.transactions:
+                if row.transactionDateTime < oldest:
+                    oldest = row.transactionDateTime
+                    oldestID = row.transactionID
+
                 marketdb.addtransaction(row.transactionDateTime,row.transactionID,row.quantity,row.typeName,row.typeID,row.price,row.clientID,row.clientName,row.stationID,row.stationName,row.transactionType,row.transactionFor)
+
+            if oldest > timegm(last.utctimetuple()):
+                print 'Need more transactions'
+                wtrans = auth.char.WalletTransactions(characterID=character.characterID, rowCount=2560, fromID=oldestID)
+
+                for row in wtrans.transactions:
+                    if row.transactionDateTime < oldest:
+                        oldest = row.transactionDateTime
+                        oldestID = row.transactionID
+
+                    marketdb.addtransaction(row.transactionDateTime,row.transactionID,row.quantity,row.typeName,row.typeID,row.price,row.clientID,row.clientName,row.stationID,row.stationName,row.transactionType,row.transactionFor)
+
+                if oldest > timegm(last.utctimetuple()):
+                    print 'Still need more transactions, to be fixed'
+
